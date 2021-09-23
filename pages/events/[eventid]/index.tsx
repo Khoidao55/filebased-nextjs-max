e1a2e2eb3634f8e5cb62057e9cbcbf5502b14600
Khoi Dao <khoidao55@gmail.com>
2021-09-23 16:34:50 -0700

    possible pages

---
+++ pages/events/[eventid]/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const SpecificEventPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Specific Event Page Page</h1>
+    </div>
+  )
+}
+
+export default SpecificEventPage;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <h1>This is the Home page</h1>
+    </div>
+  )
+}
+
+export default HomePage;
